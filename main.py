622186bcbcede58d044cadccb18f0808ed49d11c
Kevin86875 <wanghaoran.gao1@rbe.sk.ca>
2024-12-19 19:09:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+###############################################################################
+#Title: Credit Complete Project
+#Name: Kevin Gao
+#Assignment: Credit Complete Project
+#Class: Computer Science
+#Date: Sometime in my lifetime
+###############################################################################
